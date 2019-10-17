d0042b823f361b3781c4343ce31aae21b4402e6e
Pyotr Drobyshev <pa.drobyshev@gmail.com>
2019-10-17 22:11:27 +0600

    added xhr func to request data

---
+++ js/backend.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function () {
+  var DATA_URL = 'https://js.dump.academy/keksobooking/data';
+  var URL = 'https://js.dump.academy/keksobooking';
+  var STATUS_SUCCESS = 200;
+  var TIMEOUT = 10000;
+
+  var request = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT;
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS_SUCCESS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    return xhr;
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = request(onLoad, onError);
+    xhr.open('GET', DATA_URL);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var xhr = request(onLoad, onError);
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
